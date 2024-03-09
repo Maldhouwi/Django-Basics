41623bb571a62cb2de95f2c207b624e9279bcb6e
Maldhouwi <cliffeabudo@gmail.com>
2024-03-09 05:55:50 +0300

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home,name='home'),
+    path('gallery/', views.images,name='gallery'),
+    path('about/', views.about,name='about'),
+]
